22fcd1445c499e82f392cd593c2a5d92eab5de28
Armando José Ovalle <ovalle_938@hotmail.com>
2025-02-17 18:01:50 -0500

    Update About1.js

--- components/About/About1.js
+++ components/About/About1.js
@@ -57,7 +57,7 @@ const About1 = ({ clientHeight }) => {
           className="font-medium text-[2.70rem] md:text-6xl lg:text-[4rem] text-center"
         >
           <span className="about-1 leading-tight">
-            I&apos;m awordpress who&apos;s focused on building
+            I&apos;m awordpress who&apos;s WORDPRESS
             scalable and performant apps.{" "}
           </span>
           <span className="about-2 leading-tight">
